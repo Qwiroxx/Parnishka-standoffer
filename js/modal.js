ab79709b97e93e04b3f30753cf69d392655baf5d
alina464 <chudnetsovaalina@gmail.com>
2025-08-06 20:10:27 +0200

    commit-seven-alina

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+document.getElementById("open-modal-btn").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("close-my-modal-btn").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.remove("open")
+})
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("my-modal").classList.remove("open")
+    } 
+});
